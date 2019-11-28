d4e859db36bf5779760e523e5dc7608309fb2de9
Colin Weiner <colin.weiner@microsoft.com>
2019-11-27 16:54:04 -0800

    change to Footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Containers As Infrastructure
       </div>
       <div class="col-lg-6">
       </div>
